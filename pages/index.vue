9e434da938cc6056dbe0482f2511fdf0b1aa5f21
Harshal Patil <harshal.rp@gmail.com>
2019-11-02 13:02:17 +0530

    Seed Nuxt + Fable

---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <h1>Hello world!</h1>
+</template>
+
+<script>
+
+import * as x from '../src/Library.fs';
+
+export default {
+
+  mounted() {
+
+    console.log('Mounted');
+
+  }
+
+}
+</script>
