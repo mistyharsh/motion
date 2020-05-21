2bbf75c5b14c3e1c85fdfdd893a8a5bd2b3ffeb5
Harshal Patil <harshal.rp@gmail.com>
2020-05-21 22:19:32 +0530

    Introductory bindings for Preact library

--- src/app/main.js
+++ src/app/main.js
@@ -1,3 +1,4 @@
 import * as Lib from '../lib/Library.fs';
+import * as App from '../lib/App.fs';
 
 Lib.hello('Welcome Harshal');
--- src/lib/App.fs
+++ src/lib/App.fs
@@ -1,6 +1,9 @@
 
 namespace App
 
+
 module App =
 
     let main = 10
+
+    // Browser.Dom.
